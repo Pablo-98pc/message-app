953272c67212b72b3b0359aa03028e03429acc26
Sebastian M <ra3gan1991@gmail.com>
2022-03-18 00:43:30 +0100

    Fix login issues

--- src/components/helpers/getProfileByUsername.js
+++ src/components/helpers/getProfileByUsername.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const getProfileByUsername = async (username) => {
+const getProfileByUsername = async(username) => {
     console.log(username);
     let response = await axios.get(`http://localhost:3001/api/users/byusername/'${username}'`);
-    console.log(response.data); 
+    console.log(response.data);
     return response;
 }
-  export default getProfileByUsername;
+export default getProfileByUsername;
